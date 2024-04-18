f60cfb8cd4d5de5901398c26f2be5462236a5d9e
wappon28dev <wappon_28_dev@ruru.be>
2024-04-18 12:15:19 +0900

    🚚 ちょっと変更

--- src/02/issue2.c
+++ src/02/issue2.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
   int a = 5;
   int b = 3;
 
-  printf("%d / %d = %d...%d\n", a, b, a / b, a % b);
+  printf("%d / %d = %d ... %d\n", a, b, a / b, a % b);
 
   return 0;
 }
