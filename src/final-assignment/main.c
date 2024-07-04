a6e6bfd89a9a679cce200ab90dd0a52940640a18
wappon28dev <wappon_28_dev@ruru.be>
2024-07-04 15:33:53 +0900

    🚀 CMake 立ち上げ

---
+++ src/final-assignment/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, from cook-zip-c!\n");
+}
---
+++ src/final-assignment/tests/test.c
@@ -0,0 +1,8 @@
+#include "../lib/add.c"
+#include <assert.h>
+#include <stdio.h>
+
+int main()
+{
+  assert(add(1, 1) == 2);
+}
